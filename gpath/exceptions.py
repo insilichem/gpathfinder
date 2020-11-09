531ca1b2258333b66b2263255740a9f7189d7740
José-Emilio Sánchez-Aparicio <joseemilio.sanchez@uab.cat>
2020-11-09 17:46:42 +0100

    add: ligand conformations could be provided in a folder

--- gpath/exceptions.py
+++ gpath/exceptions.py
@@ -4,19 +4,19 @@
 ##############
 # GPathFinder: Identification of ligand pathways by a multi-objective
 # genetic algorithm
-# 
+#
 # https://github.com/insilichem/gpathfinder
 #
 # Copyright 2019 José-Emilio Sánchez Aparicio, Giuseppe Sciortino,
-# Daniel Villadrich Herrmannsdoerfer, Pablo Orenes Chueca, 
+# Daniel Villadrich Herrmannsdoerfer, Pablo Orenes Chueca,
 # Jaime Rodríguez-Guerra Pedregal and Jean-Didier Maréchal
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #      http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -47,4 +47,8 @@ class TooManyAtoms(Exception):
 
 
 class TooManyResidues(Exception):
+    pass
+
+
+class LigandDirectoryAndTorsion(Exception):
     pass
